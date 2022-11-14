5b131602f71f95ab5982913020b80ec6e78337c3
FlakM <maciej.jan.flak@gmail.com>
2022-11-14 20:38:26 +0100

    feature: add rss & mp3 downloader

---
+++ common/src/lib.rs
@@ -0,0 +1,68 @@
+use std::convert::TryFrom;
+
+use anyhow::anyhow;
+use anyhow::Result;
+use regex::Regex;
+use rss::Enclosure;
+use rss::{Channel, Item};
+use serde::{Deserialize, Serialize};
+
+impl TryFrom<String> for AllEpisodes {
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let channel = Channel::read_from(value.as_bytes())?;
+        println!("deserialized items: {}", channel.items.len());
+
+        let episodes: Vec<Episode> = channel
+            .items
+            .into_iter()
+            .map(|i| i.try_into())
+            .collect::<Result<_>>()?;
+        Ok(AllEpisodes{episodes})
+    }
+
+    type Error = anyhow::Error;
+}
+
+pub struct AllEpisodes {
+    pub episodes: Vec<Episode>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Episode {
+    /// id has to contain only letters: a-zA-Z0-9 and has to be unique
+    pub id: String,
+    pub title: String,
+    pub description: Option<String>,
+    pub content: Option<String>,
+    pub link: String,
+    pub enclosure: Enclosure,
+    pub pub_date: String,
+}
+
+impl TryFrom<Item> for Episode {
+    type Error = anyhow::Error;
+    fn try_from(episode: Item) -> Result<Self, Self::Error> {
+        //todo come up with better cleaning up... at least move it to lazy static
+        let re = Regex::new(r"[a-zA-Z0-9]").unwrap();
+        let title = episode.title.ok_or_else(|| anyhow!("missing title"))?;
+        let episode = Episode {
+            id: re
+                .find_iter(&title)
+                .map(|a| a.as_str())
+                .collect::<Vec<_>>()
+                .join(""),
+            title,
+            description: episode.description,
+            content: episode.content,
+            link: episode.link.ok_or_else(|| anyhow!("missing link!"))?,
+            enclosure: episode
+                .enclosure
+                .ok_or_else(|| anyhow!("missing enclosure url"))?,
+            pub_date: episode
+                .pub_date
+                .ok_or_else(|| anyhow!("missing pubValue"))?,
+        };
+
+        Ok(episode)
+    }
+}
--- search-load/src/main.rs
+++ search-load/src/main.rs
@@ -1,62 +1,16 @@
-use anyhow::anyhow;
+use std::convert::TryFrom;
+
 use anyhow::Result;
+use common::AllEpisodes;
+use common::Episode;
 use meilisearch_sdk::{settings::Settings, task_info::TaskInfo, Client};
-use regex::Regex;
-use rss::Enclosure;
-use rss::{Channel, Item};
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
-struct Episode {
-    /// id has to contain only letters: a-zA-Z0-9 and has to be unique
-    id: String,
-    title: String,
-    description: Option<String>,
-    content: Option<String>,
-    link: String,
-    enclosure: Enclosure,
-    pub_date: String,
-}
-
-impl TryFrom<Item> for Episode {
-    type Error = anyhow::Error;
-    fn try_from(episode: Item) -> Result<Self, Self::Error> {
-        //todo come up with better cleaning up... at least move it to lazy static
-        let re = Regex::new(r"[a-zA-Z0-9]").unwrap();
-        let title = episode.title.ok_or_else(|| anyhow!("missing title"))?;
-        let episode = Episode {
-            id: re
-                .find_iter(&title)
-                .map(|a| a.as_str())
-                .collect::<Vec<_>>()
-                .join(""),
-            title,
-            description: episode.description,
-            content: episode.content,
-            link: episode.link.ok_or_else(|| anyhow!("missing link!"))?,
-            enclosure: episode
-                .enclosure
-                .ok_or_else(|| anyhow!("missing enclosure url"))?,
-            pub_date: episode
-                .pub_date
-                .ok_or_else(|| anyhow!("missing pubValue"))?,
-        };
-
-        Ok(episode)
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let content = include_str!("../all_shows.xml");
-    let channel = Channel::read_from(content.as_bytes())?;
-    println!("deserialized items: {}", channel.items.len());
+    let content = include_str!("../all_shows.xml").to_string();
 
-    let episodes: Vec<Episode> = channel
-        .items
-        .into_iter()
-        .map(|i| i.try_into())
-        .collect::<Result<_>>()?;
+    let episodes: Vec<Episode> = AllEpisodes::try_from(content)?.episodes;
     let client = Client::new("http://139.144.189.54:7700", "MASTER_KEY");
 
     let indexes = client.list_all_indexes().await?;
